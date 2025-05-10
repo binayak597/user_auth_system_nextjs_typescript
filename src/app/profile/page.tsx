c8b0b94e646dc00b0db6bb3eda5f28d34220c844
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-05-10 20:32:27 +0530

    add dynamic route parameter page for profile feature

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1>Profile</h1>
+      <hr />
+    </div>
+  )
+}
+
+export default ProfilePage
