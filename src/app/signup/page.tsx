96d6e9d2ac2d25edecdd07aef5a0c512c0ca8137
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-05-10 20:21:25 +0530

    intitial setup complete and add login and singup client component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,31 +5,47 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { axios } from "axios";
 
-type LoginFormData = {
+type SignupFormData = {
+  username: string;
   email: string;
   password: string;
 };
 
-const LoginPage = () => {
-  const [loginFormData, setLoginFormData] = useState<LoginFormData>({
+const SignupPage = () => {
+  const [signupFormData, setSignupFormData] = useState<SignupFormData>({
+    username: "",
     email: "",
     password: "",
   });
 
   const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = ev.target;
-    setLoginFormData({
-      ...loginFormData,
+
+    setSignupFormData({
+      ...signupFormData,
       [name]: value,
     });
   };
 
-  const handleLogin = () => {};
+  const handleSignup = () => {};
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="mb-8">Login</h1>
+      <h1 className="mb-8">Signup</h1>
       <hr />
+
+      {/* username field*/}
+
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        type="text"
+        name="username"
+        value={signupFormData.username}
+        onChange={handleChange}
+        placeholder="Enter your username"
+        className="p-4 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
 
       {/* email field */}
 
@@ -38,10 +54,10 @@ const LoginPage = () => {
         id="email"
         type="email"
         name="email"
-        value={loginFormData.email}
+        value={signupFormData.email}
         onChange={handleChange}
         placeholder="Enter your email"
-        className="p-4 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+        className="p-4 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       />
 
       {/* password field */}
@@ -51,21 +67,21 @@ const LoginPage = () => {
         id="password"
         type="password"
         name="password"
-        value={loginFormData.password}
+        value={signupFormData.password}
         onChange={handleChange}
         placeholder="Password"
-        className="p-4 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+        className="p-4 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       />
 
       <button
-        onClick={handleLogin}
+        onClick={handleSignup}
         className="p-4 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
-        login
+        signup
       </button>
-      <Link href="/signup">Signup here</Link>
+      <Link href="/login">Login here</Link>
     </div>
   );
 };
 
-export default LoginPage;
+export default SignupPage;
---
+++ src/dbConfig/connectToDB.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () =>{
+
+  try {
+
+    const connection = await mongoose.connect(process.env.MONGODB_URI!)
+    
+
+    //registering connection events
+    connection.connection.on("connected", () => {
+      console.log("MongoDB connected successfully...")
+    })
+
+
+    connection.connection.on("error", (error) => {
+
+      console.error("MongoDB connection error, please make sure mongoDB is running " + error)
+      process.exit(1)
+    })
+    
+  } catch (error) {
+    
+    console.error("Something went wrong while connection to mongoDB")
+    console.log(error)
+    process.exit(1)
+  }
+
+}
