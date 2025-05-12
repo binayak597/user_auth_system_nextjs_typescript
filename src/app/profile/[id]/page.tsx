a05d6464d497c2ce29e7d6b559228f47dd2f908d
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-05-12 23:33:17 +0530

    fix the dynamic route parameter error in profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
+import { useParams } from "next/navigation";
 import React from "react";
 
-interface ProfilePageProps {
-  params: {
-    id: string;
-  };
-}
 
-const ProfilePage = ({ params }: ProfilePageProps) => {
+const ProfilePage = () => {
+
+  const {id} = useParams<{ id: string }>()
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile Page</h1>
@@ -16,7 +14,7 @@ const ProfilePage = ({ params }: ProfilePageProps) => {
       <p className="text-2xl py-2">
         Profile page data
         <span className="ml-2 rounded-md bg-orange-500 text-black p-2">
-          {params.id}
+          {id}
         </span>
       </p>
     </div>
